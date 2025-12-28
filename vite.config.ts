74ad4bb5c2ee18abd0653e9cc3ed71f376aeacfd
ayoubsebtv-boop <ayoubsebtv@gmail.com>
2025-12-28 20:03:21 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/iptvwebsite/', // MUST match repo name
-})
+  base: './', // Ensures GitHub Pages loads assets correctly
+});
